a220829c7372d7bfb0d2fb1d903a6836f71c7638
Vincent-FK <vincent.buso@funkey-project.com>
2021-04-26 22:55:55 +0200

    add keymap handling in FunKey menu
    Signed-off-by: Vincent-FK <vincent.buso@funkey-project.com>

--- frontend/menu.c
+++ frontend/menu.c
@@ -756,6 +756,9 @@ void run_menu_loop()
     char fname[MAXPATHLEN];
     int reset_last_scren_on_exit = 1;
 
+    /// ------ Load default keymap ------
+    system(SHELL_CMD_KEYMAP_DEFAULT);
+
     /// ------ Get init values -------
     init_menu_system_values();
     int prevItem=menuItem;
@@ -1171,6 +1174,9 @@ void run_menu_loop()
         /// --------- reset screen refresh ---------
         screen_refresh = 0;
     }
+
+    /// ------ Restore last keymap ------
+    system(SHELL_CMD_KEYMAP_RESUME);
 
     /// ------ Reset prev key repeat params -------
     if(SDL_EnableKeyRepeat(backup_key_repeat_delay, backup_key_repeat_interval)){
